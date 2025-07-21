8c6940282edee03b59e5df4a325ab69cf232a9b9
Dayeon <chiikawa143@gmail.com>
2025-07-21 16:27:32 +0900

    [feat] #6 기본 버튼 생성

--- oroom-client/src/main.ts
+++ oroom-client/src/main.ts
@@ -6,9 +6,12 @@ import { createPinia } from 'pinia'
 import App from './App.vue'
 import router from './router'
 
+import ButtonItem from './components/ButtonItem.vue'
+
 const app = createApp(App)
 
 app.use(createPinia())
 app.use(router)
 
+app.component('ButtonItem', ButtonItem)
 app.mount('#app')
