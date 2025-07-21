00e08e38c3d74d33e14b18ff53627c12abebec8b
Dayeon <chiikawa143@gmail.com>
2025-07-21 17:26:26 +0900

    [fix] 불필요한 코드 삭제

--- oroom-client/src/router/index.ts
+++ oroom-client/src/router/index.ts
@@ -6,7 +6,6 @@ import TestView from '../views/TestView.vue'
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
-
     {
       path: '/',
       name: 'home',
