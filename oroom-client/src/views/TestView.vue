c25fef7656750759bbdbd858bcc22c538645bd62
Dayeon <chiikawa143@gmail.com>
2025-07-21 20:57:16 +0900

    복구

---
+++ oroom-client/src/views/TestView.vue
@@ -0,0 +1,13 @@
+<template>
+  <ButtonItem />
+</template>
+
+<script setup>
+import ButtonItem from '../components/ButtonItem.vue'
+
+const handleClick = (event) => {
+  console.log('버튼 클릭됨!')
+}
+</script>
+
+<style scoped></style>
