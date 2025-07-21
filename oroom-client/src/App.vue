1e24f15aca22b5e34b3b6a55642907dcbdf9ebe0
Dayeon <chiikawa143@gmail.com>
2025-07-21 16:27:32 +0900

    [Feat] #6 기본 버튼 생성

--- oroom-client/src/App.vue
+++ oroom-client/src/App.vue
@@ -1,5 +1,13 @@
 <template>
-  <div></div>
+  <div>
+    <!-- 기본 버튼 -->
+    <Button @click="handleClick1" text="기본 버튼1" />
+  </div>
+
+  <div>
+    <!-- 기본 버튼 어두운 버전 -->
+    <Button variant="dark" @click="handleClick2" text="기본 버튼2" />
+  </div>
 </template>
 <script setup lang="ts"></script>
 <style scoped></style>
