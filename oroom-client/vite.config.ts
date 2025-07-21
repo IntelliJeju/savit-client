708532f28c1e19d5282356029b34a234e43f82f9
YSW2 <jrgyber@gmail.com>
2025-07-21 14:46:52 +0900

    [Feat] 모바일 환경 접근 셋팅
    - 모바일에서 [IP 주소]:5173으로 접근 가능

--- oroom-client/vite.config.ts
+++ oroom-client/vite.config.ts
@@ -7,6 +7,12 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [vue(), vueDevTools()],
+  server: {
+    host: '0.0.0.0', // 모든 IP에서 접근 가능
+    port: 5173,
+    // 선택사항: 자동으로 브라우저 열지 않기
+    open: false,
+  },
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url)),
