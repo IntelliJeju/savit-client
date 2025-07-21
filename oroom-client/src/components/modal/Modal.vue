a44b64d62ba4640a7da4f220af70fba14187cb5b
YSW2 <jrgyber@gmail.com>
2025-07-21 16:50:14 +0900

    [Feat] Modal 기본 코드 생성

---
+++ oroom-client/src/components/modal/Modal.vue
@@ -0,0 +1,26 @@
+<template>
+  <div
+    v-if="show"
+    class="modal-overlay fixed w-full h-full bg-[#00000055] flex justify-center items-center"
+  >
+    <div class="modal-container bg-white w-[70%] min-h-[20vh] rounded-lg flex">
+      <div class="modal-header"></div>
+      <div class="modal-contet"></div>
+      <div class="modal-footer"></div>
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+interface ModalProps {
+  show: boolean
+  title?: string
+}
+
+const props = withDefaults(defineProps<ModalProps>(), {
+  show: false,
+  title: '',
+})
+</script>
+
+<style scoped></style>
