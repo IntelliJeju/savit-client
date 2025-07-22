b2f4f77de4d41383eb617c2f369e3f5c809ac826
YSW2 <jrgyber@gmail.com>
2025-07-22 10:23:46 +0900

    [Feat] Modal 닫기 기능 구현
    - emit 이벤트 사용하여 close, confirm 구현

--- oroom-client/src/components/modal/Modal.vue
+++ oroom-client/src/components/modal/Modal.vue
@@ -2,11 +2,26 @@
   <div
     v-if="show"
     class="modal-overlay fixed w-full h-full bg-[#00000055] flex justify-center items-center"
+    @click="handleClose"
   >
-    <div class="modal-container bg-white w-[70%] min-h-[20vh] rounded-lg flex">
-      <div class="modal-header"></div>
-      <div class="modal-contet"></div>
-      <div class="modal-footer"></div>
+    <div
+      class="modal-container bg-white w-[70%] min-h-[20vh] rounded-lg flex flex-col p-4 gap-4"
+      @click.stop
+    >
+      <div class="modal-contet flex-1">
+        <slot></slot>
+      </div>
+      <div class="modal-footer flex justify-end gap-4">
+        <button class="close-button bg-[#F7F5FF] px-3 py-2 rounded-[0.75rem]" @click="handleClose">
+          닫기
+        </button>
+        <button
+          class="confirm-button bg-[#028174] px-3 py-2 rounded-[0.75rem] text-white"
+          @click="handleConfirm"
+        >
+          확인
+        </button>
+      </div>
     </div>
   </div>
 </template>
@@ -17,10 +32,28 @@ interface ModalProps {
   title?: string
 }
 
+interface ModalEmits {
+  'update:show': [value: boolean]
+  close: []
+  confirm: []
+}
+
 const props = withDefaults(defineProps<ModalProps>(), {
   show: false,
   title: '',
 })
+
+const emit = defineEmits<ModalEmits>()
+
+const handleConfirm = () => {
+  emit('update:show', false)
+  emit('confirm')
+}
+
+const handleClose = () => {
+  emit('update:show', false)
+  emit('close')
+}
 </script>
 
 <style scoped></style>
