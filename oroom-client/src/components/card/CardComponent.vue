481fbbc0af91c3591b604c9fed6b78af9314db38
YSW2 <jrgyber@gmail.com>
2025-07-22 13:59:02 +0900

    [Feat] 카드 컴포넌트 구현
    - 스타일링 구현

---
+++ oroom-client/src/components/card/CardComponent.vue
@@ -0,0 +1,9 @@
+<template>
+  <div class="card-container w-full min-h-16 p-4 shadow-md rounded-lg">
+    <slot></slot>
+  </div>
+</template>
+
+<script setup lang="ts"></script>
+
+<style scoped></style>
