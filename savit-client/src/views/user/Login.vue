9f8d77fb05593a45e251611b0e6e1186c89e20ef
Dayeon <chiikawa143@gmail.com>
2025-07-23 22:27:22 +0900

    [FIX] 로그인 화면 디테일 수정
    로고 그림자, 배경색, 컴포넌트 위치 수정

--- savit-client/src/views/user/Login.vue
+++ savit-client/src/views/user/Login.vue
@@ -1,20 +1,26 @@
 <template>
-  <div
-    class="flex flex-col items-center justify-center min-h-screen bg-gradient-to-t from-transparent from-5% to-[#6C5CE7]"
-  >
-    <img src="@/assets/img/savit_logo.png" alt="Savit Logo" class="w-[50%]" />
-    <br />
-    <br />
-    <br />
-    <img
-      src="@/assets/img/kakao_login_medium_wide.png"
-      alt="Kakao Login"
-      class="w-[50%]"
-      @click.prevent="handleKakaoLogin"
-    />
+  <div class="min-h-screen bg-gradient-to-t from-[#333333] to-[#0AB68B] flex flex-col">
+    <div class="flex-1"></div>
+    <div class="flex-1 flex items-center justify-center">
+      <img
+        src="@/assets/img/savit_logo.png"
+        alt="Savit Logo"
+        class="w-[70%] drop-shadow-[10px_10px_10px_rgba(0,0,0,0.5)]"
+      />
+    </div>
+    <div class="flex-1 flex items-end justify-center pb-16">
+      <div class="flex flex-col items-center w-[70%]">
+        <div class="w-full border-t border-[#61A292] mb-8" />
+        <img
+          src="@/assets/img/kakao_login_medium_wide.png"
+          alt="Kakao Login"
+          class="w-full"
+          @click.prevent="handleKakaoLogin"
+        />
+      </div>
+    </div>
   </div>
 </template>
-W
 
 <script setup lang="ts">
 import { useAuthStore } from '@/stores/auth'
