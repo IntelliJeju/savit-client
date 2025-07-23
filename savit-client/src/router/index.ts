d78fa95768dcf223026728b61f6c2f31519e0ff6
YSW2 <jrgyber@gmail.com>
2025-07-23 10:56:26 +0900

    [Feat] #36 기본 라우팅 구조 구현
    - 기능별로 라우팅 분할, 메타데이터 설정
    - lazy loading 방식 사용

--- savit-client/src/router/index.ts
+++ savit-client/src/router/index.ts
@@ -1,13 +1,120 @@
 import { createRouter, createWebHistory } from 'vue-router'
-// import HomeView from '../views/HomeView.vue'
+
+const MainLayout = () => import('@/layout/MainLayout.vue')
+
+// Index
+const DashBoard = () => import('@/views/DashBoard.vue')
+
+// Auth
+const Login = () => import('@/views/user/Login.vue')
+
+// Card
+const CardCurrent = () => import('@/views/card/CardCurrent.vue')
+const CardRegister = () => import('@/views/card/CardRegister.vue')
+
+// Budget
+const BudgetCheck = () => import('@/views/budget/BudgetCheck.vue')
+const BudgetSetting = () => import('@/views/budget/BudgetSetting.vue')
+
+// Challenge
+const ChallengeMain = () => import('@/views/challenge/ChallengeMain.vue')
+const ChallengeCurrent = () => import('@/views/challenge/ChallengeCurrent.vue')
+const ChallengeDetail = () => import('@/views/challenge/ChallengeDetail.vue')
+const ChallengeStatistics = () => import('@/views/challenge/ChallengeStatistics.vue')
+const ChallengeResult = () => import('@/views/challenge/ChallengeResult.vue')
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
-      path: '/login',
-      name: 'login',
-      component: () => import('@/views/user/Login.vue'),
+      path: '/auth',
+      component: MainLayout,
+      meta: { showNavigation: false, requiresAuth: false },
+      children: [
+        {
+          path: 'login',
+          name: 'Login',
+          component: Login,
+        },
+      ],
+    },
+    {
+      path: '/',
+      component: MainLayout,
+      meta: { showNavigation: true, requiresAuth: true },
+      children: [
+        {
+          path: '',
+          name: 'Dashboard',
+          component: DashBoard,
+        },
+      ],
+    },
+    {
+      path: '/card',
+      component: MainLayout,
+      meta: { showNavigation: true, requiresAuth: true },
+      children: [
+        {
+          path: 'current',
+          name: 'CardCurrent',
+          component: CardCurrent,
+        },
+        {
+          path: 'register',
+          name: 'CardRegister',
+          component: CardRegister,
+        },
+      ],
+    },
+    {
+      path: '/budget',
+      component: MainLayout,
+      meta: { showNavigation: true, requiresAuth: true },
+      children: [
+        {
+          path: 'check',
+          name: 'BudgetCheck',
+          component: BudgetCheck,
+        },
+        {
+          path: 'setting',
+          name: 'BudgetSetting',
+          component: BudgetSetting,
+        },
+      ],
+    },
+    {
+      path: '/challenge',
+      component: MainLayout,
+      meta: { showNavigation: true, requiresAuth: true },
+      children: [
+        {
+          path: '',
+          name: 'ChallengeMain',
+          component: ChallengeMain,
+        },
+        {
+          path: 'current',
+          name: 'ChallengeCurrent',
+          component: ChallengeCurrent,
+        },
+        {
+          path: 'detail',
+          name: 'ChallengeDetail',
+          component: ChallengeDetail,
+        },
+        {
+          path: 'statistics',
+          name: 'ChallengeStatistics',
+          component: ChallengeStatistics,
+        },
+        {
+          path: 'result',
+          name: 'ChallengeResult',
+          component: ChallengeResult,
+        },
+      ],
     },
   ],
 })
