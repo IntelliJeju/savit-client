a2a69afb77c8f48d776af5dddad8ab4d643d5ec6
YSW2 <jrgyber@gmail.com>
2025-07-24 16:58:27 +0900

    [Feat] 색상 상수 추가
    - 필요한 색상 상수로 사용하도록 구현

--- savit-client/tailwind.config.js
+++ savit-client/tailwind.config.js
@@ -2,7 +2,18 @@
 export default {
   content: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        // 커스텀 색상 정의
+        'app-green': '#0AB68B',
+        'app-light-green': '#92DE8B',
+        'app-dark-green': '#028174',
+        'app-red': '#FE4444',
+        'app-yellow': '#FFBC00',
+        'app-purple': '#6C5CE7',
+        'app-dark-gray': '#545045',
+      },
+    },
   },
   plugins: [],
 }
