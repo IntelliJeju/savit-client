511e75e1731fa11cfa750eb3cf5f9032884de978
YSW2 <jrgyber@gmail.com>
2025-07-30 14:06:31 +0900

    [Feat] #71 앱 시작, 새로고침 시 로그인 상태 복원 코드 추가

--- savit-client/src/App.vue
+++ savit-client/src/App.vue
@@ -5,7 +5,15 @@
 </template>
 
 <script setup lang="ts">
+import { onMounted } from 'vue'
 import MainLayout from './layout/MainLayout.vue'
+import { useAuthStore } from './stores/auth.ts'
+
+const authStore = useAuthStore()
+
+onMounted(() => {
+  authStore.restoreAuthentication()
+})
 </script>
 
 <style scoped></style>
